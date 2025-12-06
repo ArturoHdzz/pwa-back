db2ff569c9876e4c135b162b22b24d2010702cd2
VictoriaReyes <victoria.jaime@silverstone-tech.com>
2025-12-05 18:15:59 -0600

    WebPushService

--- app/Http/Controllers/WebPushController.php
+++ app/Http/Controllers/WebPushController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\WebPushSubscription;
-use App\services\WebPushService;
+use App\Services\WebPushService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
